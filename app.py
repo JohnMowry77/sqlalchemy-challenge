24e041434428c3b64152110d3a0b9763270cec7f
John Mowry <72773479+JohnMowry77@users.noreply.github.com>
2021-02-24 21:27:52 -0600

    Surfs Up

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
-
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///Resources/hawii.sqlite")
 
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
 # Save reference to the table
-#Measurement=Base.classes.measurement
+Measurement=Base.classes.measurement
 Station=Base.classes.station
 
+session=Session(engine)
 #################################################
 # Flask Setup
 #################################################
@@ -29,16 +28,18 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-
 @app.route("/")
 def home():
     print("server received a request for 'Home' page")
     return "Surfs Up!"
 
+
+
 all_precipitation=[]
 @app.route("/api/v1.0/precipitation")
 def prcp():
-    result_prcp=sesion.query(Measurement.date, Measurement.prcp)
+    result_prcp=session.query(Measurement.date, Measurement.prcp)
+    
     precpitation={}
     for x in result_prcp:
         precpitation["date"]= date
@@ -51,5 +52,6 @@ def prcp():
 
 
 
-
+if __name__=='__main__':
+    app.run(debug=True)
 
